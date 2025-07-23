71fdef3f5724a601f32e9c9e790caaf9b57c76b4
piermars <pierthethief@gmail.com>
2025-07-23 13:16:03 +0200

    Backup Scripts

--- Corso CodeMonkey + aggiunte mie/Assets/Scripts/Giocatore.cs
+++ Corso CodeMonkey + aggiunte mie/Assets/Scripts/Giocatore.cs
@@ -6,6 +6,8 @@ public class Giocatore : MonoBehaviour {
     // Rendere moveSpeed pubblico può causare malfunzionamento con altre classi che utilizzano la stessa variabile, motivo per la quale è più sicuro e comodo renderla private e inserirci SerializeField
     [SerializeField] private float moveSpeed = 6.5f; // Può essere comodo renderla pubblica all'inizio giusto per far comparire il valore nell'editor e trovare più comodamente la velocità adatta
     
+    private bool staCamminando; // Variabile che servirà sotto per l'animazione di camminata
+    
     // Movimento del personaggio
     private void Update() { // Update is called once per frame
         Vector2 inputVettore = new Vector2(0, 0); // Creiamo prima Vector2 per raccogliere gli input da tastiera orizzontale e verticale (WASD)
@@ -27,8 +29,13 @@ public class Giocatore : MonoBehaviour {
         Vector3 dirGiocatore = new Vector3(inputVettore.x, 0f, inputVettore.y); // Creiamo questo vettore poiché la posizione del giocatore va tra x,y,z e non solo x,y
         transform.position += dirGiocatore * moveSpeed * Time.deltaTime; // Con l'ultimo costrutto, evitiamo che la velocità di un giocatore vada in base ai suoi fps
 
+        staCamminando = dirGiocatore != Vector3.zero; // il giocatore si muove se la direzione del giocatore è diversa dal vettore zero
+
         // Rotazione giocatore
         float rotateSpeed = 10f; // Velocità di rotazione
         transform.forward = Vector3.Slerp(transform.forward, dirGiocatore, Time.deltaTime * rotateSpeed);
+    }
+    public bool StaCamminando() {
+        return staCamminando;
     }
 }
--- Corso CodeMonkey + aggiunte mie/Assets/Scripts/GiocatoreAnimazione.cs
+++ Corso CodeMonkey + aggiunte mie/Assets/Scripts/GiocatoreAnimazione.cs
@@ -5,10 +5,13 @@ public class GiocatoreAnimazione : MonoBehaviour
 
     private const string STA_CAMMINANDO = "StaCamminando"; // Così facendo siamo più sicuri quando scriviamo la componente all'ultima riga perché, se sbagliamo a scrivere, ce lo sottolinea di rosso
 
+    [SerializeField] private Giocatore giocatore; // Abbiamo serializzato perché in questo modo nell'Editor abbiamo inserito lo script Giocatore.cs nella Visual insieme a questo script, in questo modo possiamo riferirci alla classe Giocatore attraverso la variabile giocatore
 
     private Animator animator; // Leggere la documentazione
     private void Awake() {
-        animator = GetComponent<Animator>(); // Abbiamo un riferimento al nostro componente Animator (parametro?)
-        animator.SetBool(STA_CAMMINANDO, // continuare da 1:38:11 circa
+        animator = GetComponent<Animator>(); // Abbiamo un riferimento al nostro componente Animator
+    }
+    private void Update() {
+        animator.SetBool(STA_CAMMINANDO, giocatore.StaCamminando());  // "StaCamminando" è vero se il metodo invocato è vero, altrimenti è falso e quindi non sta camminando
     }
 }
